aa31fbdfa858b0d716ba693c1a8f3232d119842d
Rupampiku <piku.mondal2010@gmail.com>
2017-06-11 22:11:52 +0530

    2nd Tutorial

---
+++ tscript.js
@@ -0,0 +1,23 @@
+var name = 'John';
+var age = 26;
+
+
+var job, isMarried;
+
+
+
+job = 'teacher';
+isMarried = false;
+
+console.log(name + ' is a ' + age + ' years old ' + job + '. Is he married? ' + isMarried + '.');
+
+age = 'thirty six';
+job = 'driver';
+
+console.log(name + ' is a ' + age + ' years old ' + job + '. Is he married? ' + isMarried + '.');
+
+var lastName = prompt('What is the last name?');
+console.log(lastName);
+
+
+alert(name + ' is a ' + age + ' years old ' + job + '. Is he married? ' + isMarried + '.');
